f7061d70d4eb806108514e46d58b6a54c77fb702
ora78officials <ora78officials@gmail.com>
2026-06-20 00:18:11 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,15 @@ const port = process.env.PORT || 3000;
 
 http.createServer((req, res) => {
   fs.readFile('index.html', (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end('File not found');
+      return;
+    }
+
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end(data);
   });
-}).listen(port);
+}).listen(port, '0.0.0.0');
 
-console.log(`Server running on ${port}`);
+console.log(`Server running on port ${port}`);
